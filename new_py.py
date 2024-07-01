38ac3657a958f496638df5356c7349b9d7c2046d
firdavsboysunov04@gmil.com <firdavsboysunov04@gmail.com>
2024-07-01 17:15:45 +0500

    connected database

--- new_py.py
+++ new_py.py
@@ -1,5 +1,27 @@
-# import psycopg2
+import psycopg2
 
-# conn = psycopg2.connect(database='postgres', user='post', host='localhost', )
+def connect():
 
-print('hello')
+    with psycopg2.connect(database = 'sql', 
+                            user='postgres', 
+                            password='112',  
+                            host='localhost', 
+                            port= 5432) as conn:
+        return conn
+
+def select_of_department():
+    conn = connect()
+    select_all_from_depatment = '''select name as name, id as department_id FROM department;'''
+    with conn.cursor() as cursor:
+        cursor.execute(select_all_from_depatment)
+        data = cursor.fetchall()
+    print(data)
+
+def select_of_employee():
+    conn = connect()
+    select_name_and_id_from_employee = '''SELECT employee_name as employe_name, id as employe_id FROM employee;'''
+    with conn.cursor() as cursor:
+        cursor.execute(select_name_and_id_from_employee)
+        data = cursor.fetchall()
+    print(data)
+select_of_department()
